4b2decb05bc44bcab58b38c40281cf8b2d9b65f9
zhernick <jchernikj@gmail.com>
2020-09-27 19:23:47 +0300

    Добавил слайдер и блок для него

---
+++ pulse/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        autoplay: false,
+        autoplaySpeed: 1500,
+        speed: 500,
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/right.svg"></button>',
+
+    });
+  });
