fd07cf2506d820e0d21fdfda944c3ca0a463aeea
zhernick <jchernikj@gmail.com>
2020-10-02 21:01:08 +0300

    сделал кнопки работающие

--- pulse/js/main.js
+++ pulse/js/main.js
@@ -47,7 +47,26 @@ $(document).ready(function () {
       hamburger.classList.toggle('hamburger__content_active');
       menu.classList.toggle('footer__info_active');
     });
-  })
+  });
+
+  //modal
+
+  $('[data-modal="consultation"]').on('click', function () {
+    $('.overlay, #consultation').fadeIn();
+  });
+
+  $('.modal__close').on('click', function () {
+    $('.overlay, #consultation, #thanks, #order').fadeOut();
+  });
+
+  $('.button_mini').each(function (i) {
+    $(this).on('click', function () {
+      $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+      $('.overlay, #order').fadeIn();
+    });
+  });
+
+
 
 });
 
