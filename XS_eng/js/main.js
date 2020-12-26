34f9104bd417b1f710758aeabffbaecda6d6f4ad
zhernick <71762095+zhernick@users.noreply.github.com>
2020-12-26 16:38:59 +0200

    Add files via upload

--- XS_eng/js/main.js
+++ XS_eng/js/main.js
@@ -6,23 +6,28 @@ const tree = document.querySelector('.tree');
 const body = document.body;
 
 //card rotate on mouse move
-container.addEventListener('mousemove', (rotate) =>{
+// container.addEventListener('mousemove', (rotate) =>{
+container.addEventListener('touchmove', (rotate) =>{
     rotate.preventDefault();
 
-    let x = (innerWidth / 2 - rotate.pageX) /15;
-    let y = (innerHeight / 2 - rotate.pageY) /15;
+    // let x = (innerWidth / 2 - rotate.pageX) /15;
+    // let y = (innerHeight / 2 - rotate.pageY) /15;
+    let x = (innerWidth / 2 - event.changedTouches[0].pageX) / 15;
+    let y = (innerHeight / 2 - event.changedTouches[0].pageY) / 15;
 
     card.style.transform = "rotateY("+ x + "deg) rotateX("+ y + "deg)";
 })
 
-container.addEventListener('mouseover', () =>{
-    title.style.transform = 'translate3d(0,0,180px)';
-    santa.style.transform = 'translate3d(0,0,250px)';
-    tree.style.transform = 'translate3d(0,0,100px)';
+// container.addEventListener('mouseover', () =>{
+container.addEventListener('touchstart', () =>{
+    title.style.transform = 'translate3d(0,0,130px)';
+    santa.style.transform = 'translate3d(0,0,160px)';
+    tree.style.transform = 'translate3d(0,0,90px)';
     card.style.transition = 'all 0.1s ease';
 });
 
-container.addEventListener('mouseout', () =>{
+// container.addEventListener('mouseout', () =>{
+container.addEventListener('touchend', () =>{
     title.style.transform = 'translate3d(0,0,0)';
     santa.style.transform = 'translate3d(0,0,0)';
     tree.style.transform = 'translate3d(0,0,0)';
