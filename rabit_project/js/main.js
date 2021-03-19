591dc6249d35f52acb952cd215bfb3f9a7f00e2e
zhernick <71762095+zhernick@users.noreply.github.com>
2021-03-19 19:15:27 +0200

    Add files via upload

---
+++ rabit_project/js/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+let btn = document.querySelector('.btn');
+let rabbit = document.querySelector('.rabit');
+
+btn.addEventListener('click', function(){
+    rabbit.classList.toggle('rabit_active');
+    rabbit.style.toggle('animation-play-state: paused');
+})
