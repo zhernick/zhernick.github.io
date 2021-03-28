0fb03c921cb5bff894db6473ce197d0b5bb69910
zhernick <71762095+zhernick@users.noreply.github.com>
2021-03-28 16:22:55 +0300

    Add files via upload

--- citata/js/main.js
+++ citata/js/main.js
@@ -1,22 +1,32 @@
 'use strict';
 
 
-let arr;
 // let ara = [];
 // let answers = prompt('Введите цитаты, через запятую (, )').split(', ');
 // ara = answers;
 
-arr = ['Это тебе не это, мать твою', 'Любишь кушать - люби и не кушать', 'Сделал дело - сделай еще дело', 'Адреналин стекал в ботинки…', 'Как ни крутись - а жопа сзади', 'Ума нет - считай коллега', 'Жизнь грустная, зато зарплата смешная', 'Раз, два, три - больше бегай, меньше жри!', 'Если твоя жена курица, тогда ты точно не орёл', 'Стрельба без причины - признак Аль Пачины'];
 // arr = [...arr, ...ara];
 // console.log(arr);
 
-let span = document.querySelector('span');
+// let span = document.querySelector('span');
 
-let random = Math.floor(Math.random() * arr.length);
-let btn = document.querySelector('button');
-btn.addEventListener('click', function(){
-    span.innerHTML = arr[random];
-});
+// let random = Math.floor(Math.random() * arr.length);
+// let btn = document.querySelector('button');
+// btn.addEventListener('click', function(){
+//     span.innerHTML = arr[random];
+// });
+
+Array.prototype.rand = function () {
+    return this[Math.floor(Math.random() * this.length)];
+}
+
+
+let arr = ['ШО - вместо тысячи слов','Любишь кушать - люби и не кушать', 'Сделал дело - сделай еще дело', 'Как ни крутись - а жопа сзади', 'Ума нет - считай коллега', 'Жизнь грустная, зато зарплата смешная', 'Раз, два, три - больше бегай, меньше жри!', 'Если твоя жена курица, тогда ты точно не орёл', 'Стрельба без причины - признак Аль Пачины'];
+const btn = document.getElementById('button');
+const div = document.getElementById('div');
+btn.onclick = function () {
+    div.innerHTML = arr.rand();
+};
 
 
 
